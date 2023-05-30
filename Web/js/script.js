1a49521c73d5a4bf97c51b14d6fd84be6d287702
Samuel Asher Rivello <63511769+SamuelAsherRivello@users.noreply.github.com>
2023-05-30 14:45:39 +0300

    Add web demo

---
+++ Web/js/script.js
@@ -0,0 +1,17 @@
+var count = 0;
+
+document.getElementById('clickMeButton').addEventListener('click', function() 
+{
+    // Update count
+    count = count + 1;
+
+    // Create message
+    var buttonLabel = document.getElementById('clickMeButton').innerText;
+    var message = `You clicked the '${buttonLabel}' button ${count} times.`;
+
+    // Show message offcreen (for debugging)
+    console.log(message);
+
+    // Show message onscreen (for end user)
+    document.getElementById('statusText').textContent = message;
+});
