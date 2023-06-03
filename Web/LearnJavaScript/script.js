9125cc6e25aac66814b67707371bb3cf4e3464db
Samuel Asher Rivello <63511769+SamuelAsherRivello@users.noreply.github.com>
2023-06-03 09:55:26 +0300

    Update web examples

---
+++ Web/LearnJavaScript/script.js
@@ -0,0 +1,28 @@
+
+// Comments begin with "//"
+
+// Variables
+let name = 'Tammy';
+var count = 0;
+var message = `The value of count is ${count}.`;
+
+// Math
+count = count + 1;
+count = count - 1;
+count = count * 10;
+count = count / 10;
+
+
+// Debugging
+console.log(message);
+
+
+//Function (Also called "Methods")
+//Declare A Function
+function HelloWorld ()
+{
+    console.log("This is inside the function!");
+}
+
+//Call A Function
+HelloWorld();
