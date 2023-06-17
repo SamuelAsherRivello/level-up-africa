ae5b722ff4bea8512de3756a36ab47b1842f4767
Samuel Asher Rivello <63511769+SamuelAsherRivello@users.noreply.github.com>
2023-06-17 15:34:26 +0300

    Add layoutcomponent

---
+++ Unity/Assets/Examples/More/UIToolkit/Approaches/Example_02_UIToolkit_LayoutComponent/Example_02_UIToolkit_LayoutComponent.cs
@@ -0,0 +1,135 @@
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+// ReSharper disable ArrangeAccessorOwnerBody
+namespace RMC.LevelUpAfrica.Examples.More.UIToolkit.Approaches.Example_02_UIToolkit_LayoutComponent
+{
+    //  Namespace Properties ------------------------------
+
+    //  Class Attributes ----------------------------------
+
+    /// <summary>
+    /// Demonstrates a **RAW** approach to using UIToolkit.
+    /// </summary>
+    public class Example_02_UIToolkit_LayoutComponent : MonoBehaviour
+    {
+        //  Events ----------------------------------------
+
+
+        //  Properties ------------------------------------
+        public float Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+                _value = Mathf.Clamp(_value, 0, 100);
+                _barFill.style.right = new StyleLength(new Length(100-_value, LengthUnit.Percent));
+            }
+        }
+        
+        public string Title
+        {
+            get
+            {
+                return _titleLabel.text;
+            }
+            set
+            {
+                _titleLabel.text = value;
+            }
+        }
+
+        public string Details
+        {
+            get
+            {
+                return _detailsLabel.text;
+            }
+            set
+            {
+    
+                _detailsLabel.text = value;
+            }
+        }
+        
+
+        //  Fields ----------------------------------------
+        [SerializeField]
+        private UIDocument _uiDocument;
+
+        private Label _titleLabel;
+        private Label _detailsLabel;
+        private VisualElement _icon;
+        private VisualElement _barFill;
+        private float _value;
+        private CancellationTokenSource _cancellationToken;
+
+        //  Unity Methods ---------------------------------
+        protected void Start()
+        {
+            // Query
+            _titleLabel = _uiDocument.rootVisualElement.Q<Label>("TitleLabel");
+            _detailsLabel = _uiDocument.rootVisualElement.Q<Label>("DetailsLabel");
+            _icon = _uiDocument.rootVisualElement.Q<VisualElement>("Icon");
+            _barFill = _uiDocument.rootVisualElement.Q<VisualElement>("BarFill");
+            
+            // Events
+            _icon.RegisterCallback<ClickEvent>(Icon_OnClicked);
+            
+            // Defaults
+            UpdateValue(50);
+
+            //Instructions
+            Debug.Log("Click the StatsBar Icon to change health.");
+
+        }
+
+        //  Methods ---------------------------------------
+        private void UpdateValue(float nextValue)
+        {
+            Value = nextValue;
+            Title = $"Health";
+            Details = $"{Value:0}%";    
+        }
+
+        
+        //  Event Handlers --------------------------------
+        private async void Icon_OnClicked(ClickEvent evt)
+        {
+            float fromValue = Value;
+            float toValue = fromValue + 10;
+            int durationMilliseconds = 500; 
+            
+            if (toValue >= 100)
+            {
+                toValue = 0;
+            }
+
+            //LERP: If we are already changing the value, cancel
+            //the old change and use only the new change.
+            if (_cancellationToken != null)
+            {
+                _cancellationToken.Cancel();
+            }
+            _cancellationToken = new CancellationTokenSource();
+            
+            //Update the value over time for demonstration purposes
+            await LerpHelper.LerpValueAsync(
+                fromValue, 
+                toValue, 
+                durationMilliseconds, 
+                _cancellationToken,
+                (nextValue) =>
+                {
+                    UpdateValue(nextValue);
+                });
+        }
+
+
+    }
+}
